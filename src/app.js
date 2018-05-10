12fe65702d0c33f67361a2ef65773c03a3bdb3e4
HaydenEdgmon <howtodointernet@gmail.com>
2018-05-10 13:57:51 -0500

    testing compilation

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 export default class App extends Component{
     render(){
         return (
-            <div>Hello</div>
+            <div>Hello I exist</div>
     );
     }
 }
