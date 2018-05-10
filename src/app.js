f3c353838ff40db79988cb536897c74839766116
HaydenEdgmon <howtodointernet@gmail.com>
2018-05-10 13:46:04 -0500

    re testing commits

--- src/app.js
+++ src/app.js
@@ -9,4 +9,4 @@ export default class App extends Component{
     }
 }
 
-ReactDOM.render(<App message="Hooray, I exist!"/>, document.getElementById("main))
+ReactDOM.render(<App message="Hooray, I exist!"/>, document.getElementById("main"))
